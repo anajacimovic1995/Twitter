cd874c5f23951d8651243bc109a47c38aa7306f3
anajacimovic1995 <anajac95@gmail.com>
2016-04-08 01:01:55 +0200

    TwitterTest is made without errors.

--- Twitter/test/com/twitter/TwitterTest.java
+++ Twitter/test/com/twitter/TwitterTest.java
@@ -21,61 +21,63 @@ public class TwitterTest {
 	public void tearDown() throws Exception {
 		twitter = null;
 	}
+
 	@Test
 	public void testVratiSvePoruke() {
 		twitter.vratiSvePoruke();
 	}
 
-	@Test(expected = java.lang.RuntimeException.class)
+	@Test (expected = java.lang.RuntimeException.class)
 	public void testUnesiNull() {
 		String korisnik = null;
 		String poruka = null;
 		twitter.unesi(korisnik, poruka);
 	}
-
-	@Test(expected = java.lang.RuntimeException.class)
+	
+	@Test (expected = java.lang.RuntimeException.class)
 	public void testUnesiKorisnikEmptyString() {
 		String korisnik = "";
 		String poruka = "";
 		for (int i = 0; i < 150; i++) {
 			poruka += "a";
 		}
-		twitter.unesi(korisnik, poruka);
+		twitter.unesi(korisnik, poruka);	
+		
 	}
-
-	@Test(expected = java.lang.RuntimeException.class)
+	
+	@Test (expected = java.lang.RuntimeException.class)
 	public void testUnesiMoreThan140() {
 		String korisnik = "Petar Petrovic";
 		String poruka = "";
 		for (int i = 0; i < 150; i++) {
 			poruka += "a";
 		}
-		twitter.unesi(korisnik, poruka);
+		twitter.unesi(korisnik, poruka);	
 	}
-
+	
 	@Test
 	public void testUnesiRightMessage() {
 		String korisnik = "Petar Petrovic";
 		String poruka = "Good morning!";
-		twitter.unesi(korisnik, poruka);
 		TwitterPoruka tp = new TwitterPoruka();
+		twitter.unesi(korisnik, poruka);
 		tp.setKorisnik(korisnik);
 		tp.setPoruka(poruka);
 		assertEquals(tp.toString(), twitter.vratiSvePoruke().getLast().toString());
 	}
 	
-	@Test(expected = java.lang.RuntimeException.class)
+	@Test (expected = java.lang.RuntimeException.class)
 	public void testVratiPorukeNullTag() {
 		String tag = null;
 		twitter.vratiPoruke(5, tag);
 	}
 
-	@Test(expected = java.lang.RuntimeException.class)
+	@Test (expected = java.lang.RuntimeException.class)
 	public void testVratiPorukeEmptyStringTag() {
 		String tag = "";
 		twitter.vratiPoruke(5, tag);
 	}
-
+	
 	@Test
 	public void testVratiPorukeLessOrEqualsZero() {
 		String korisnik = "Petar Petrovic";
@@ -85,11 +87,10 @@ public class TwitterTest {
 		}
 		twitter.vratiPoruke(0, poruka);
 	}
-
+	
 	@Test
 	public void testVratiPorukeRightMessage() {
 		String tag = "Morning!";
 		twitter.vratiPoruke(15, tag);
 	}
-
 }
